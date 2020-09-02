be3fdb076ccd06d0ebeb109a826f42914b97e751
chaosgrimmon <31082757+chaosgrimmon@users.noreply.github.com>
2020-09-01 21:40:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os, discord, random
+import os
+import discord
+import random
 from discord.ext import commands
 
 TOKEN = os.environ['DISCORD_TOKEN']
@@ -9,3 +11,21 @@ pTokens = {}
 
 emojiTokens = ['↖️', '↗️', '↘️', '↙️', '🤖']
 playerLock = False
+
+readyNum = 0
+members = {}
+
+@client.event
+async def on_ready():
+    print("Bot is ready!")
+    players.clear()
+    
+@client.event
+async def on_reaction_add(reaction, user):
+    if reaction.message.author != client.user:
+        return
+    if user == client.user:
+        return
+
+    
+client.run('token')
